96b9c003b9fed1dbcf54e2fd8445701261c83306
shinobushiva <stagesp1@gmail.com>
2022-05-15 02:39:46 +0900

    マイグレーションの追加

---
+++ src/database.ts
@@ -0,0 +1,28 @@
+import Migration from './lib/migration';
+import { Context, Callback } from 'aws-lambda';
+import ormconfig from './config/ormconfig';
+
+const success = (response: any) => ({
+  statusCode: 200,
+  body: JSON.stringify(response),
+});
+
+const handler =
+  (handlerName: string) =>
+  async (event: any, context: Context, callback: Callback) => {
+    context.callbackWaitsForEmptyEventLoop = false;
+
+    const migration = new Migration(ormconfig);
+
+    try {
+      const response = await migration[handlerName]();
+      callback(null, success(response));
+    } catch (error) {
+      callback(error);
+    }
+  };
+
+const migrationUp = handler('runMigration');
+const migrationDown = handler('undoLastMigration');
+
+export { migrationUp, migrationDown };
